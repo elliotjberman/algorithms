d4c19ef88b8bf0fe227cf380c25ebff23adf262f
elliotjberman <elliotjberman@gmail.com>
2016-04-18 16:07:41 -0400

    first homework

---
+++ find_inversions.py
@@ -0,0 +1,41 @@
+global count
+count = 0
+
+content = []
+with open('IntegerArray.txt') as f:
+	for line in f:
+		content.append(int(line))
+
+def count_inversions(array):
+	if len(array) > 1:
+		middle  = len(array)//2
+		a = array[:middle]
+		b = array[middle:]
+
+		count_inversions(a)
+		count_inversions(b)
+
+		i = 0
+		j = 0
+		k = 0
+		while i < len(a) and j < len(b):
+			if a[i] < b[j]:
+				array[k] = a[i]
+				i = i+1
+			else:
+				array[k] = b[j]
+				j = j+1
+				global count
+				count = count + len(a)-i
+			k = k+1
+		while i < len(a):
+			array[k] = a[i]
+			i = i+1
+			k = k+1
+		while j < len(b):
+			array[k] = b[j]
+			j = j+1
+			k = k+1
+
+count_inversions(content)
+print(count)
---
+++ mergesort.py
@@ -0,0 +1,32 @@
+def merge_sort(array):
+	if len(array) > 1:
+		middle  = len(array)//2
+		a = array[:middle]
+		b = array[middle:]
+
+		merge_sort(a)
+		merge_sort(b)
+
+		i = 0
+		j = 0
+		k = 0
+		while i < len(a) and j < len(b):
+			if a[i] < b[j]:
+				array[k] = a[i]
+				i = i+1
+			else:
+				array[k] = b[j]
+				j = j+1
+			k = k+1
+		while i < len(a):
+			array[k] = a[i]
+			i = i+1
+			k = k+1
+		while j < len(b):
+			array[k] = b[j]
+			j = j+1
+			k = k+1
+	return array
+	
+input_array = [5, 4, 1, 8, 7, 2, 6, 3]
+print(merge_sort(input_array))
