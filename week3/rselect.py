c8055ec5319ef268cca99dcd498ec2c78fe0efa3
elliotjberman <elliotjberman@gmail.com>
2016-04-27 09:51:50 -0400

    rselect

---
+++ week3/rselect.py
@@ -0,0 +1,32 @@
+from random import randint
+
+def swap(array, a, b):
+	array[a], array[b] = array[b], array[a]
+
+def random_select(array, order_statistic, first, last):
+	if order_statistic < 1 or order_statistic > len(array):
+		print('Order statistic out of range')
+		return 'Error'
+	if last-first > 0:
+		pivot_index = randint(first, last-1)
+		pivot = array[pivot_index]
+		swap(array, first, pivot_index)
+
+		i = first + 1
+		j = first + 1
+
+		while j < last:
+			if array[j] < pivot:
+				swap(array, j, i)
+				i+=1
+			j+=1
+		swap(array, i-1, first)
+
+		if i > len(array) - order_statistic+1:
+			return random_select(array, order_statistic, first, i-1)
+		elif i < len(array) - order_statistic+1:
+			return random_select(array, order_statistic, i, last)
+		else:
+			return array[i-1]
+	else:
+		return array[first]
